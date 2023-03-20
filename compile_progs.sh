09d8ad8c1839cee892c7d98533a347346c2c2c95
Hokanosekai <arsouille.raillard@gmail.com>
2023-03-20 18:52:11 +0100

    Added compile for others programs

--- compile_progs.sh
+++ compile_progs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+mkdir -p ./bin
+
 cd ./progs/MonPG1
 make
 cd ../MonPG2
 make
+gcc -Wall ../MonPG3.c -o ../../bin/MonPG3
+gcc -Wall ../MonPG4.c -o ../../bin/MonPG4
+gcc -Wall ../MonPG5.c -o ../../bin/MonPG5
